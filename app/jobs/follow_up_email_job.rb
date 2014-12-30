1b1f97e29e1615b36308d12856c2dc08fc4653b7
Isaac Seymour <isaac@gocardless.com>
2014-12-30 18:37:09 +0000

    Use serialize/deserialize methods on ActiveJob::Base rather than manipulating arguments

--- app/jobs/follow_up_email_job.rb
+++ app/jobs/follow_up_email_job.rb
@@ -9,17 +9,13 @@ class FollowUpEmailJob < ActiveJob::Base
     # When this is 5 or greater it will get run 5 times and then fail
     # when this is less than 5 the job will raise that many times, be retried,
     # and then succeed
-    raise "Oh no" unless @attempt_number > 5
+    raise "Oh no" unless @attempt_number > 4
     UserMailer.follow_up_email(email).deliver_now
   end
 
   ##############
   # Retry code #
   ##############
-  before_enqueue { |job| job.arguments << (@attempt_number || 0) + 1 }
-
-  before_perform { |job| @attempt_number = job.arguments.pop }
-
   rescue_from(Exception) { |ex| retry_or_raise(ex) }
 
   def backoff_strategy
@@ -35,4 +31,15 @@ class FollowUpEmailJob < ActiveJob::Base
     puts "Retrying in #{backoff_strategy[@attempt_number]}"
     retry_job(wait: backoff_strategy[@attempt_number])
   end
+
+  def serialize
+    super.merge('attempt_number' => (@attempt_number || 0) + 1)
+  end
+
+  def deserialize(job_data)
+    super(job_data)
+    self.attempt_number = job_data['attempt_number']
+  end
+
+  attr_accessor :attempt_number
 end
